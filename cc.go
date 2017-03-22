5d6296aa2b56128c53356cdf971d4ca9d57071af
nik0405 <nik.dba09@gmail.com>
2017-03-22 20:31:16 +0530

    Create cc.go

---
+++ cc.go
@@ -0,0 +1,261 @@
+/*GlobalPayment POC */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// SimpleChaincode example to simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+//Init Method to initialize the state to ledgers
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Init called, initializing chaincode")
+	
+	var custName string  	//Customer Name
+	var currentBal int 		//Customer Balance
+	var err error
+
+	//Error for wrong input
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2 arguments -customer name and customer current balance")
+	}
+
+	// Initialize the chaincode
+	custName = args[0]
+	currentBal, err = strconv.Atoi(args[1])
+	//Validate type for balance
+	if err != nil {
+		return nil, errors.New("Expecting integer value for balance")
+	}
+	
+	fmt.Printf("currentBal = %d \n", currentBal)
+
+	// Write the state to the ledger
+	err = stub.PutState(custName, []byte(strconv.Itoa(currentBal)))
+		if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+//Credit Function
+//Expecting customer name and credit amount
+func (t *SimpleChaincode) credit(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running credit")
+	
+	var custName string  					  // Customer Name
+	var currentBal int 						  //Customer Balance  --Aval->currentBal
+	var creditAmount int         		    //Credit Amount
+	var err error
+
+	//Error for wrong input
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2- customer name and credit amount")
+	}
+
+	custName = args[0]
+	
+	// Get the current state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	
+	custCurrentBalbytes , err := stub.GetState(custName)  
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if custCurrentBalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	currentBal, _ = strconv.Atoi(string(custCurrentBalbytes))
+
+	// Bvalbytes, err := stub.GetState(B)
+	// if err != nil {
+	// 	return nil, errors.New("Failed to get state")
+	// }
+	// if Bvalbytes == nil {
+	// 	return nil, errors.New("Entity not found")
+	// }
+	// Bval, _ = strconv.Atoi(string(Bvalbytes))
+
+	// Credit Execution
+	creditAmount, err = strconv.Atoi(args[1])
+	currentBal = currentBal + creditAmount
+	// Bval = Bval + X
+	fmt.Printf("currentBal = %d\n", currentBal)
+
+	// Write the state back to the ledger
+	err = stub.PutState(custName, []byte(strconv.Itoa(currentBal)))
+	if err != nil {
+		return nil, err
+	}
+
+	// err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return nil, nil
+}
+
+//Expecting customer name and credit amount
+func (t *SimpleChaincode) debit(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running credit")
+	
+	var custName string    // Customer Name
+	var currentBal int //Customer Balance  --Aval->currentBal
+	var debitAmount int          // Transaction value  X->creditAmount
+	var err error
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	custName = args[0]
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	custCurrentBalbytes , err := stub.GetState(custName)  //Avalbytes ->custCurrentBalbytes
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if custCurrentBalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	currentBal, _ = strconv.Atoi(string(custCurrentBalbytes))
+
+	// Debit Execution
+	debitAmount, err = strconv.Atoi(args[1])
+	currentBal = currentBal - debitAmount
+	
+	fmt.Printf("currentBal = %d\n", currentBal)
+
+	// Write the state back to the ledger
+	err = stub.PutState(custName, []byte(strconv.Itoa(currentBal)))
+	if err != nil {
+		return nil, err
+	}
+
+	// err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return nil, nil
+}
+
+// Deletes a customer from state
+func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running deletion of customer")
+	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1 customer name")
+	}
+
+	cust := args[0]
+
+	// Delete the key from the state in ledger
+	err := stub.DelState(cust)
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
+	return nil, nil
+}
+
+// Invoke callback representing the invocation of a chaincode
+// This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Invoke called, determining function")
+	
+	// Handle different functions
+	if function == "credit" {
+		// Transaction makes a credit to the customer
+		fmt.Printf("Function is credit")
+		return t.credit(stub, args)
+	} else if function == "debit" {
+		// Transaction makes a debit from the customer
+		fmt.Printf("Function is debit")
+		return t.debit(stub, args)
+	}else if function == "init" {
+		fmt.Printf("Function is init")
+		return t.Init(stub, function, args)
+	} else if function == "delete" {
+		// Deletes an customer from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function invocation")
+}
+
+func (t* SimpleChaincode) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Run called, passing through to Invoke (same function)")
+	
+	// Handle different functions
+	if function == "credit" {
+		// Transaction makes a credit to the customer
+		fmt.Printf("Function is credit")
+		return t.credit(stub, args)
+	}else if function == "debit" {
+		// Transaction makes a debit from the customer
+		fmt.Printf("Function is debit")
+		return t.debit(stub, args)
+	}else if function == "init" {
+		fmt.Printf("Function is init")
+		return t.Init(stub, function, args)
+	}else if function == "delete" {
+		// Deletes an entity from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function invocation")
+}
+
+// Query callback representing the query of a chaincode
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Query called, determining function")
+	
+	if function != "query" {
+		fmt.Printf("Function is query")
+		return nil, errors.New("Invalid query function name. Expecting \"query\"")
+	}
+	var custName string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	custName = args[0]
+
+	// Get the state from the ledger
+	custAvailBalbytes, err := stub.GetState(custName)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + custName + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if custAvailBalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + custName + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + custName + "\",\"Amount\":\"" + string(custAvailBalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return custAvailBalbytes, nil
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
